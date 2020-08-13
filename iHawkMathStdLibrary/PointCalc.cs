e6db52e4c8a51e876b1e4b28277a8545452acdca
DavidHawkes <db300@163.com>
2020-08-13 15:33:37 +0800

    Update PointCalc.cs

--- iHawkMathStdLibrary/PointCalc.cs
+++ iHawkMathStdLibrary/PointCalc.cs
@@ -47,5 +47,65 @@ namespace iHawkMathStdLibrary
             var point = GetMidPointF(p1, p2);
             return new Point((int) (point.X + 0.5), (int) (point.Y + 0.5));
         }
+
+        /// <summary>
+        /// 获取某一向量上距离起点某一距离的点
+        /// </summary>
+        /// <param name="pt1">向量起点</param>
+        /// <param name="pt2">向量终点</param>
+        /// <param name="distance">距离</param>
+        /// <returns>所求点</returns>
+        public static PointF GetPointOnVector(PointF pt1, PointF pt2, float distance)
+        {
+            var d = (float) Math.Sqrt((pt2.X - pt1.X) * (pt2.X - pt1.X) + (pt2.Y - pt1.Y) * (pt2.Y - pt1.Y));
+            var t = distance / d;
+            float x = t * pt2.X + (1 - t) * pt1.X, y = t * pt2.Y + (1 - t) * pt1.Y;
+            return new PointF(x, y);
+        }
+
+        /// <summary>
+        /// 获取某一向量终点旋转某一角度（角度值）后的点
+        /// </summary>
+        /// <param name="pt1">向量起点</param>
+        /// <param name="pt2">向量终点</param>
+        /// <param name="angle">旋转角度（角度值）</param>
+        /// <returns>所求点</returns>
+        public static PointF GetRotatedPointOnVector(PointF pt1, PointF pt2, float angle)
+        {
+            float dx = pt2.X - pt1.X, dy = pt2.Y - pt1.Y;
+            float
+                dx1 = (float) (dx * Math.Cos(angle * Math.PI / 180) - dy * Math.Sin(angle * Math.PI / 180)),
+                dy1 = (float) (dx * Math.Sin(angle * Math.PI / 180) + dy * Math.Cos(angle * Math.PI / 180));
+            return new PointF(pt1.X + dx1, pt1.Y + dy1); //pt2绕pt1旋转后的点
+        }
+
+        /// <summary>
+        /// 获取某一向量上距离起点某一距离的点，并绕起点旋转某一角度（角度值）后的点
+        /// </summary>
+        /// <param name="pt1">向量起点</param>
+        /// <param name="pt2">向量终点</param>
+        /// <param name="distance">距离</param>
+        /// <param name="angle">旋转角度（角度值）</param>
+        /// <returns>所求点</returns>
+        public static PointF GetRotatedPointOnVector(PointF pt1, PointF pt2, float distance, float angle)
+        {
+            return GetRotatedPointOnVector(pt1, GetPointOnVector(pt1, pt2, distance), angle);
+        }
+
+        /// <summary>
+        /// 获取某一向量终点倾斜某一角度（角度值）后的点
+        /// </summary>
+        /// <param name="pt1">向量起点</param>
+        /// <param name="pt2">向量终点</param>
+        /// <param name="d">旋转角度（角度值）</param>
+        /// <param name="dn">倾斜参照角度（角度值）</param>
+        /// <returns>所求点</returns>
+        public static PointF GetLeanedPointOnVector(PointF pt1, PointF pt2, float d, float dn = 0.0F)
+        {
+            var pt22 = GetRotatedPointOnVector(pt1, pt2, d); //pt2绕pt1旋转后的点
+            if (Math.Abs(dn - 0) <= 0) return new PointF(pt22.X, pt2.Y); //水平倾斜
+            if (Math.Abs(dn - 90) <= 0) return new PointF(pt2.X, pt22.Y); //垂直倾斜
+            return PointF.Empty;
+        }
     }
 }
