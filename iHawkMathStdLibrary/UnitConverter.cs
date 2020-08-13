26bbd91cb5bdefd4dfad84308a31db6b91ca61a7
DavidHawkes <db300@163.com>
2020-08-13 15:32:40 +0800

    Create UnitConverter.cs

---
+++ iHawkMathStdLibrary/UnitConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iHawkMathStdLibrary
+{
+    /// <summary>
+    /// 单位换算
+    /// </summary>
+    public static class UnitConverter
+    {
+        /// <summary>
+        /// 像素(px)转换成厘米(cm)
+        /// </summary>
+        /// <param name="px">像素值</param>
+        /// <param name="dpi">分辨率</param>
+        /// <returns>厘米值</returns>
+        public static float Px2Cm(float px, float dpi)
+        {
+            return (float) (px / dpi * 2.539999918);
+        }
+    }
+}
